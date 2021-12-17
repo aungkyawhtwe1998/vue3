23474af10b4e575fd4861f8e5a0f820f70eeeb95
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2021-12-17 21:55:51 +0630

    conditional rendering

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Socks',
-            description: "This is the description"
+            description: "This is the description",
+            image : './assets/images/socks_blue.jpg',
+            inventory: 0,
+            onSale: true
         }
     }
 })
