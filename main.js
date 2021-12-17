3b3f7a9c1416cc2e09b216bb69984765a97eeced
aungkyawhtwe1998 <aungkyawhtwe.mdy49@gmail.com>
2021-12-17 22:36:49 +0630

    event handling

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const app = Vue.createApp({
     data(){
         return{
+            cart:0,
             product: 'Socks',
             description: "This is the description",
             image : './assets/images/socks_blue.jpg',
             inventory: 0,
-            onSale: true
+            onSale: true,
+            variants:[
+                {
+                    id:2234, color:'green', image:'./assets/images/socks_green.jpg'
+                },
+                {
+                    id:2235, color:'blue', image:'./assets/images/socks_blue.jpg'
+                }
+            ]
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
